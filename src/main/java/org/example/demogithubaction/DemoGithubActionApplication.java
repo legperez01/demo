87f57549c170f2eb5da8c7e64e5427a133fc3d81
Luis Gonzalez <legperez@gmail.com>
2024-05-07 10:26:06 -0400

    initial commit

---
+++ src/main/java/org/example/demogithubaction/DemoGithubActionApplication.java
@@ -0,0 +1,13 @@
+package org.example.demogithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithubActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/demogithubaction/DemoGithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demogithubaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
